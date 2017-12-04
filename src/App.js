0d4032576675bf6dc016df2f58e0f787a5757872
Toni Bardina <tonibardina@gmail.com>
2017-12-04 11:20:21 +0100

    creating canvas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <canvas id='workspace' width='490' height='220' />
+      </div>
+    )
+  }
+}
+
+export default App
